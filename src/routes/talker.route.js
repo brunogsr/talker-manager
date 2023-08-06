a63d2b2c2a631f994800f6b1f1d1807e2f8991a7
brunogsr <brunogsr https://github.com/brunogsr>
2023-08-06 11:24:57 -0300

    corrigindo req 1 e recomeçando projeto

--- src/routes/talker.route.js
+++ src/routes/talker.route.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const fs = require('fs').promises; // corrigindo para módulo fs, conforme o README
 
 const serverTalker = express.Router();
+const path = require('path');
 
-const talkers = require('../talker.json');
+const pathTalker = path.join(__dirname, '..', 'talker.json');
 
-serverTalker.get('/', (_req, res) => {
-  res.status(200).json(talkers);
+serverTalker.get('/', async (_req, res) => {
+    const data = await fs.readFile(pathTalker, 'utf-8');
+    const talkers = JSON.parse(data);
+    //
+    if (!talkers || talkers.length === 0) {
+      res.status(200).json([]);
+    }
+    res.status(200).json(talkers);
 });
 
 module.exports = serverTalker;
